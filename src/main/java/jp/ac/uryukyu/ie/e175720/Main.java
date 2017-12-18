8180b597e4701dcfe48c390c1eab988004f43de3
Matsuda Masami <e175720@ie.u-ryukyu.ac.jp>
2017-12-18 10:30:20 +0900

    Pokerクラスの作成。

--- src/main/java/jp/ac/uryukyu/ie/e175720/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e175720/Main.java
@@ -1,8 +1,27 @@
 package jp.ac.uryukyu.ie.e175720;
 
 public class Main {
+    private static int coin;
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        coin = 1000;
+        System.out.println("カジノへようこそ\n所持チップ：" + coin);
+    }
 
+    public void selectGame(){
+        int selectgame = 0;
+        System.out.println("どのゲームで遊びますか？\n1：ポーカー");
+        if (selectgame==1){
+            
+        }
+
+    }
+
+    public int getCoin(){
+        return coin;
+    }
+
+    public void plusCoin(int coin) {
+        coin += this.coin;
     }
 }
---
+++ src/main/java/jp/ac/uryukyu/ie/e175720/Poker.java
@@ -0,0 +1,5 @@
+package jp.ac.uryukyu.ie.e175720;
+
+public class Poker {
+
+}
