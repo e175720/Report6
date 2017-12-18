9d40990b40723b7478a0ffbed21545849cb9fb0b
Matsuda Masami <e175720@ie.u-ryukyu.ac.jp>
2017-12-18 23:19:31 +0900

    Pokerクラス、コインの増減に関するメソッドの作成。

--- src/main/java/jp/ac/uryukyu/ie/e175720/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e175720/Main.java
@@ -3,16 +3,21 @@ package jp.ac.uryukyu.ie.e175720;
 public class Main {
     private static int coin;
 
+    //初期設定。
     public static void main(String[] args) {
         coin = 1000;
         System.out.println("カジノへようこそ\n所持チップ：" + coin);
     }
 
+    //ゲームの選択。
     public void selectGame(){
-        int selectgame = 0;
+        int selectgame = 1;
+        //selectgameはあとでユーザー入力ができるようにする。
         System.out.println("どのゲームで遊びますか？\n1：ポーカー");
         if (selectgame==1){
-            
+            Poker poker = new Poker(coin);
+            poker.soloPlay();
+            betCoin(50);
         }
 
     }
@@ -21,7 +26,22 @@ public class Main {
         return coin;
     }
 
+
+    //コインをいくら賭けたか表示するメソッド。
+    public void betCoin(int coin){
+        coin -= this.coin;
+        System.out.println("コインを"+ this.coin +"賭けました。");
+    }
+
+    //コインの増減を表示するメソッド。
     public void plusCoin(int coin) {
         coin += this.coin;
+        if(this.coin>0){
+            System.out.println("コインを"+ this.coin+ "獲得しました。");
+        }else if(this.coin<0){
+            System.out.println("コインを"+ this.coin + "失いました。");
+        }
+
+
     }
 }
--- src/main/java/jp/ac/uryukyu/ie/e175720/Poker.java
+++ src/main/java/jp/ac/uryukyu/ie/e175720/Poker.java
@@ -1,5 +1,22 @@
 package jp.ac.uryukyu.ie.e175720;
 
 public class Poker {
+    private int coin;
 
+    public Poker(int coin){
+        coin = this.coin;
+    }
+
+    public void game(){
+        System.out.println("ポーカーへようこそ。");
+
+
+    }
+
+    //1人プレイ。
+    public void soloPlay(){
+        System.out.println("コインを賭けて頂きます。");
+
+
+    }
 }
