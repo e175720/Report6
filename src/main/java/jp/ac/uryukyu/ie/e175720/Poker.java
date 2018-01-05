f777013c4dd0cfe1c2b339120cedfe07a11f6548
Matsuda Masami <e175720@ie.u-ryukyu.ac.jp>
2018-01-05 15:20:13 +0900

    ポーカーの実装途中。カードの配布の模索で断念。詳細はREADME.mdに。

--- src/main/java/jp/ac/uryukyu/ie/e175720/Poker.java
+++ src/main/java/jp/ac/uryukyu/ie/e175720/Poker.java
@@ -1,22 +1,41 @@
 package jp.ac.uryukyu.ie.e175720;
+import java.math.*;
 
 public class Poker {
     private int coin;
+    private int[][] hands;
+    private int[] same;
+
 
     public Poker(int coin){
         coin = this.coin;
     }
 
     public void game(){
-        System.out.println("ポーカーへようこそ。");
+        System.out.println("カードを配ります。");
+        same = new int[2];
+        hands = new int[5][2];
+        for(int i=0; i<5; i++){
+            hands[i][0] = drawNumber();
+            hands[i][1] = drawMark();
 
+        }
 
     }
 
-    //1人プレイ。
-    public void soloPlay(){
-        System.out.println("コインを賭けて頂きます。");
+    public int drawNumber(){
+        int draw_num = (int)(Math.random() * 13);
+        int card = draw_num;
+        return card;
+    }
 
+    public int drawMark(){
+        int draw_mark = (int)(Math.random() * 4);
+        int card = draw_mark;
+        return card;
+    }
 
+    public boolean sameDraw(int[] same_card){
+        
     }
 }
