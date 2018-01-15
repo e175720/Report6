abdb8affb5c42174eeb8468ee0f89971813ec66f
Matsuda Masami <e175720@ie.u-ryukyu.ac.jp>
2018-01-15 17:10:40 +0900

    カードの生成の仕方を変更。変更前をコミット。

---
+++ src/main/java/jp/ac/uryukyu/ie/e175720/Card.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e175720;
+
+public class Card {
+    int num;
+    char mark;
+
+    public Card(int num,char mark){
+        num = this.num;
+        mark = this.mark;
+    }
+}
--- src/main/java/jp/ac/uryukyu/ie/e175720/Poker.java
+++ src/main/java/jp/ac/uryukyu/ie/e175720/Poker.java
@@ -4,7 +4,7 @@ import java.math.*;
 public class Poker {
     private int coin;
     private String[] hands;
-    private String same;
+    private String[] same;
     private boolean samej = false;
     private String change;
 
@@ -15,12 +15,13 @@ public class Poker {
     public void game(){
         System.out.println("カードを配ります。");
         hands = new String[5];
+        same = new String[10];
         for(int i=0; i<5; i++){
             hands[i] = drawCard();
+            same[i] = hands[i];
             if (i>=1){
-                for(int j=0; j<i; j++){
-                    if(hands[j]==hands[i]){
-                        hands[i] = null;
+                for(int j=0;j<i;j++){
+                    if(same[j]==hands[i]){
                         samej = true;
                     }
                 }
@@ -34,9 +35,26 @@ public class Poker {
             System.out.println(look+1 + ":" + hands[look]);
         }
         System.out.println("交換するカードを記入してください。例：1,3  2,4,5");
+        //changeはあとでユーザーが入力できるようにする。
         change = "1,2";
-        
+        String[] num = change.split(",",0);
+        for(int k=0;k<num.length;k++){
+            hands[k] = drawCard();
+            for(int l=0;l<5+k;l++){
+                if(same[l]==hands[k]){
+                    samej = true;
+                }
+            }
+            if(samej){
+                k = k-1;
+                samej = false;
+            }
 
+        }
+        for(int look = 0;look<5; look++) {
+            System.out.println(look + 1 + ":" + hands[look]);
+        }
+        result(hands);
     }
 
     public String drawCard(){
@@ -48,5 +66,25 @@ public class Poker {
         int select = (int)(Math.random() * 53);
         String got = card[select];
         return got;
+    }
+
+    public void result(String[] take){
+
+        String m1;
+        String n1;
+        for(int m=0;m<5;m++){
+            String[] takes = take[m].split("", 0);
+            if(m==0){
+                m1 = takes[0];
+                n1 = takes[1];
+            }else{
+                if(m1==takes[0]){
+
+                }
+            }
+        }
+
+
+
     }
 }
